54cdc81dbe4cdd6185f59f3db5b1bd1059b7e9d1
IDA Kenichiro <ida.kenichiro@gmail.com>
2025-08-11 15:44:39 +0900

    Remove unused files

--- db/schema.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE prefectures (id INTEGER PRIMARY KEY, prefecture_name TEXT NOT NULL);
-CREATE TABLE cities (id TEXT PRIMARY KEY, city_name TEXT NOT NULL, prefecture_id INTEGER NOT NULL, FOREIGN KEY(prefecture_id) REFERENCES prefectures(id));
