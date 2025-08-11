ad77b93a3cb4ed7f0cde7407998b9431e0d1323b
IDA Kenichiro <ida.kenichiro@gmail.com>
2025-08-11 15:57:15 +0900

    Add Mantine

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,46 +1,12 @@
-import { useState } from "react";
-import { WeatherByLocationResponse } from "../types";
+import "@mantine/core/styles.css";
 
-function App() {
-  const [weather, setWeather] = useState<WeatherByLocationResponse | null>(
-    null
-  );
+import { MantineProvider } from "@mantine/core";
+import { Weather } from "./Weather";
 
-  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const cityName = formData.get("cityName");
-
-    if (typeof cityName === "string") {
-      const url = new URL("/weather", window.location.origin);
-      url.searchParams.set("q", cityName);
-
-      const response = await fetch(url.toString());
-
-      setWeather(await response.json());
-    }
-  };
-
+export default function App() {
   return (
-    <>
-      <form onSubmit={submitForm}>
-        <input type="text" name="cityName" placeholder="都市名を入力" />
-        <button type="submit">検索</button>
-      </form>
-      {weather && (
-        <div>
-          <h3>{weather.locationName}</h3>
-          <h1>{weather.hourly[0].temperature.current}°</h1>
-          <p>{weather.hourly[0].forecast}</p>
-          <p>
-            ↑{weather.daily[0].temperature.max}°/↓
-            {weather.daily[0].temperature.min}°
-          </p>
-          <p>体感温度{weather.hourly[0].temperature.apparent}°</p>
-        </div>
-      )}
-    </>
+    <MantineProvider>
+      <Weather />
+    </MantineProvider>
   );
 }
-
-export default App;
