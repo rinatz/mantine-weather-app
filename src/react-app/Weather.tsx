48e9dd0088d94bb52340bc95e6ef219b82ec4068
IDA Kenichiro <ida.kenichiro@gmail.com>
2025-08-11 16:32:54 +0900

    Use Mantine components

--- src/react-app/Weather.tsx
+++ src/react-app/Weather.tsx
@@ -1,44 +1,48 @@
 import { useState } from "react";
 import { WeatherByLocationResponse } from "../types";
+import { Box, Stack, TextInput, Title, Text } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
 
 export function Weather() {
   const [weather, setWeather] = useState<WeatherByLocationResponse | null>(
     null
   );
 
-  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const cityName = formData.get("cityName");
-
-    if (typeof cityName === "string") {
-      const url = new URL("/weather", window.location.origin);
-      url.searchParams.set("q", cityName);
-
-      const response = await fetch(url.toString());
-
-      setWeather(await response.json());
+  const fetchWeather = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== "Enter") {
+      return;
     }
+    const locationName = e.currentTarget.value;
+
+    const url = new URL("/weather", window.location.origin);
+    url.searchParams.set("q", locationName);
+
+    const response = await fetch(url.toString());
+
+    setWeather(await response.json());
   };
 
   return (
-    <>
-      <form onSubmit={submitForm}>
-        <input type="text" name="cityName" placeholder="都市名を入力" />
-        <button type="submit">検索</button>
-      </form>
-      {weather && (
-        <div>
-          <h3>{weather.locationName}</h3>
-          <h1>{weather.hourly[0].temperature.current}°</h1>
-          <p>{weather.hourly[0].forecast}</p>
-          <p>
-            ↑{weather.daily[0].temperature.max}°/↓
-            {weather.daily[0].temperature.min}°
-          </p>
-          <p>体感温度{weather.hourly[0].temperature.apparent}°</p>
-        </div>
-      )}
-    </>
+    <Stack align="center" justify="center" pt={100}>
+      <Box w={640}>
+        <TextInput
+          placeholder="地名を入力"
+          leftSection={<IconSearch />}
+          onKeyDown={fetchWeather}
+        />
+        {weather && (
+          <Stack mt={20}>
+            <Text>{weather.locationName}</Text>
+            <Title order={1}>{weather.hourly[0].temperature.current}°</Title>
+            <Text>{weather.hourly[0].forecast}</Text>
+            <Text>
+              ↑{weather.daily[0].temperature.max}°/↓
+              {weather.daily[0].temperature.min}°
+            </Text>
+            <Text>体感温度{weather.hourly[0].temperature.apparent}°</Text>
+          </Stack>
+        )}
+      </Box>
+    </Stack>
   );
 }
