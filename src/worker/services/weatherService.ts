727b97ecffa40dcbd154259de4ff932890ba019b
IDA Kenichiro <ida.kenichiro@gmail.com>
2025-08-13 17:17:45 +0900

    Not return past weather

--- src/worker/services/weatherService.ts
+++ src/worker/services/weatherService.ts
@@ -3,6 +3,8 @@
 import { fetchLocation } from "../external/nominatim";
 import { fetchWeatherForecast } from "../external/openMeteo";
 import { WeatherByLocationResponse } from "../../types";
+import { parseISO } from "date-fns";
+import { TZDate } from "@date-fns/tz";
 
 export async function getWeatherByLocation(
   locationName: string
@@ -15,35 +17,65 @@ export async function getWeatherByLocation(
     longitude: parseFloat(lon),
   });
 
+  const now = new TZDate(new Date(), weatherForecast.timezone);
+
   return {
     locationName: name,
-    hourly: weatherForecast.hourly.time.map((time, index) => {
-      return {
-        time,
-        weatherCode: weatherForecast.hourly.weather_code[index],
-        temperature: {
-          current: Math.round(weatherForecast.hourly.temperature_2m[index]),
-          apparent: Math.round(
-            weatherForecast.hourly.apparent_temperature[index]
+    hourly: weatherForecast.hourly.time
+      .filter((time) => {
+        const dateTimeISO = new TZDate(
+          parseISO(time),
+          weatherForecast.timezone
+        );
+
+        return dateTimeISO >= now;
+      })
+      .map((time, index) => {
+        const dateTimeISO = new TZDate(
+          parseISO(time),
+          weatherForecast.timezone
+        );
+
+        return {
+          time: dateTimeISO.toISOString(),
+          weatherCode: weatherForecast.hourly.weather_code[index],
+          temperature: {
+            current: Math.round(weatherForecast.hourly.temperature_2m[index]),
+            apparent: Math.round(
+              weatherForecast.hourly.apparent_temperature[index]
+            ),
+          },
+          precipitationProbability: Math.round(
+            weatherForecast.hourly.precipitation_probability[index]
           ),
-        },
-        precipitationProbability: Math.round(
-          weatherForecast.hourly.precipitation_probability[index]
-        ),
-      };
-    }),
-    daily: weatherForecast.daily.time.map((time, index) => {
-      return {
-        time,
-        weatherCode: weatherForecast.daily.weather_code[index],
-        temperature: {
-          max: Math.round(weatherForecast.daily.temperature_2m_max[index]),
-          min: Math.round(weatherForecast.daily.temperature_2m_min[index]),
-        },
-        precipitationProbability: Math.round(
-          weatherForecast.daily.precipitation_probability_max[index]
-        ),
-      };
-    }),
+        };
+      }),
+    daily: weatherForecast.daily.time
+      .filter((time) => {
+        const dateTimeISO = new TZDate(
+          parseISO(time),
+          weatherForecast.timezone
+        );
+
+        return dateTimeISO >= now;
+      })
+      .map((time, index) => {
+        const dateTimeISO = new TZDate(
+          parseISO(time),
+          weatherForecast.timezone
+        );
+
+        return {
+          time: dateTimeISO.toISOString(),
+          weatherCode: weatherForecast.daily.weather_code[index],
+          temperature: {
+            max: Math.round(weatherForecast.daily.temperature_2m_max[index]),
+            min: Math.round(weatherForecast.daily.temperature_2m_min[index]),
+          },
+          precipitationProbability: Math.round(
+            weatherForecast.daily.precipitation_probability_max[index]
+          ),
+        };
+      }),
   };
 }
